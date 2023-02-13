cc7fb0db052bd31198ce082c8b9e773b21d18b0f
Serenkij-Volk <stanok16k20f3@gmail.com>
2023-02-13 20:54:57 +0200

    Home work #1, work with objects and object propperties

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
 "use sctrict";
 
-let incr = 10,
-    decr = 10;
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?",2); // создаём опрос пользователя
 
-console.log(incr++);
-console.log(decr--);
+let personalMovieDB = {                                             // обьект для хранения данных о пользователе
+    count : numberOfFilms,                                          // количество фильмов
+    movies : {},                                                    // фильмы
+    actors : {},
+    geners : [],
+    privat : false   
+};
 
-console.log(incr);
-console.log(decr);
+let film1 = prompt("Один из последних просмотренных фильмов?", 'matrix');
+let gradeFilm1 = prompt("На сколько оцените его?", 8);
+let film2 = prompt("Один из последних просмотренных фильмов?", 'logan');
+let gradeFilm2 = prompt("На сколько оцените его?", 6);
 
+personalMovieDB.movies[film1] = gradeFilm1;
+personalMovieDB.movies[film2] = gradeFilm2;
+
+console.log(personalMovieDB.movies);
