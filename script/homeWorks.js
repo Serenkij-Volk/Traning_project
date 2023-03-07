ee2a70dc7a67f50842229c3c1511ff0e32023f59
Serenkij-Volk <stanok16k20f3@gmail.com>
2023-03-07 21:34:16 +0200

    Home work 7. Functions exercise

--- script/homeWorks.js
+++ script/homeWorks.js
@@ -64,21 +64,79 @@ let result = '';
  console.log(result);
 
 
- function getMathResult(base,count) {
+function getMathResult(base,count) {
     let string='';
     let num=0;
-    for (let i =0;i<=count;i++){
+    for (let i=0;i<count;i++){
         num+=base;
         string+=num;
-        if (i=count){
-            continue
+        if (i==count-1){
+            continue;
         }else {
             string+="---";
         }
-        console.log(string);
+        
     }
+    if (typeof count === !"number" || count===0 || count<0){
+        
+        return base;
+    } else {
     return string;
+    }
 
 }
 
-console.log(getMathResult(3,10));
+console.log(getMathResult(5,3));
+
+
+function calculateVolumeAndArea(Qside) {
+    
+    if (Qside==String || Qside<=0 || !Number.isInteger(Qside)){      
+        return "При вычислении произошла ошибка";
+    }else {
+
+        V=Qside*Qside*Qside;
+        S=Qside*Qside*6;
+
+        return 'Объем куба: '+V+', площадь всей поверхности: '+S;
+    }
+}
+
+console.log(calculateVolumeAndArea(5));
+
+
+
+function getCoupeNumber(NPlace) {
+    
+    if (NPlace==String || NPlace<=0 || !Number.isInteger(NPlace)){   
+           
+        return "Ошибка. Проверьте правильность введенного номера места";
+
+    }else {
+
+        if (NPlace<=0 || NPlace>36){
+            return "Таких мест в вагоне не существует"
+        }else if (NPlace>=1 && NPlace<=4){
+            return 1
+        }else if(NPlace>=5 && NPlace<=8){
+            return 2
+        }else if(NPlace>=9 && NPlace<=12){
+            return 3
+        }else if(NPlace>=13 && NPlace<=16){
+            return 4
+        }else if(NPlace>=17 && NPlace<=20){
+            return 5
+        }else if(NPlace>=21 && NPlace<=24){
+            return 6
+        }else if(NPlace>=25 && NPlace<=28){
+            return 7
+        }else if(NPlace>=29 && NPlace<=32){
+            return 8
+        }else if(NPlace>=33 && NPlace<=36){
+            return 9
+        }
+
+    }
+}
+
+console.log(calculateVolumeAndArea(5));
